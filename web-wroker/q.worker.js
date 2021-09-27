c3061dabb1da9877099786ef4053e7460cc0f594
tangjian <tangjian1891@163.com>
2021-09-27 22:31:19 +0800

    worker

--- web-wroker/q.worker.js
+++ web-wroker/q.worker.js
@@ -1,3 +1,14 @@
-self.addEventListener('message', function (e) {
-  self.postMessage('You said: ' + e.data);
-}, false);
+// self.addEventListener('message', function (e) {
+// }, false);
+
+
+
+let index=0
+console.time()
+while(index<10000000000){
+    index++
+    console.log(index)
+}
+console.log('结束了')
+// console.log(index);
+console.timeEnd()
