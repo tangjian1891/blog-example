728c2f83f3d392bf158e99f6f490ba40d796994f
tangjian1891 <tangjian1891@163.com>
2021-09-28 14:57:18 +0800

    worker测试

---
+++ web-wroker/public/q.worker.js
@@ -0,0 +1,35 @@
+this.window = {}; //模拟对象，便于挂载axios
+// let str = importScripts("./axios.min.js");
+let str = importScripts("https://cdn.jsdelivr.net/npm/axios@0.21.4/dist/axios.min.js");
+console.log(window.axios);
+self.addEventListener(
+  "message",
+  (e) => {
+     
+    foo();
+  },
+  false
+);
+
+function foo() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/test");
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log("成功获取数据");
+      self.postMessage("ok" + xhr.responseText + xhr.status);
+    }
+  };
+  xhr.send();
+
+  // window.axios.request({
+  //   url:"/test",
+  //   data:{
+  //     address:"你好啊"
+  //   }
+  // }).then(res=>{
+  //   console.log(res)
+  //   console.log(res.status)
+  //   self.postMessage("ok" + res.data+res.status)
+  // })
+}
---
+++ web-wroker/阻塞/zuse.worker.js
@@ -0,0 +1,7 @@
+console.log('woker执行了')
+
+
+setInterval(() => {
+  console.log(1)
+  
+}, 1000);
